b3d013b2b02bc68151b09dc3e9b4bc657a3dca5b
tandensgn <lhpt12061998study@gmail.com>
2020-04-21 01:22:35 +0700

    update 1.0.1

--- DACN_SalePhone/Controllers/HomeController.cs
+++ DACN_SalePhone/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace DACN_SalePhone.Controllers
 
 
         [ChildActionOnly]
-        public ActionResult Header()
+        public ActionResult Categories()
         {
             qlbdtDbEntities db = new qlbdtDbEntities();
             var categories = from i in db.categories
@@ -58,6 +58,37 @@ namespace DACN_SalePhone.Controllers
                                cateID = i.cate_id
                            };
             return View(products);
+        }
+        public ActionResult ProductDetail()
+        {
+            qlbdtDbEntities db = new qlbdtDbEntities();
+            var productDetail = from i in db.products
+                           select new ShowProduct()
+                           {
+                               productID = i.prod_id,
+                               productName = i.prod_name,
+                               productPrice = i.prod_price,
+                               productImage = i.prod_image,
+                               productWarranty = i.prod_warranty,
+                               productAccessories = i.prod_accessories,
+                               productCondition = i.prod_condition,
+                               productPromotion = i.prod_promotion,
+                               productStatus = i.prod_status,
+                               productDescription = i.prod_description,
+                               productFeatured = i.prod_featured,
+                               productScreen = i.prod_screen,
+                               productOs = i.prod_os,
+                               productCamf = i.prod_camf,
+                               productCamr = i.prod_camr,
+                               productCpu = i.prod_cpu,
+                               productRam = i.prod_ram,
+                               productImemory = i.prod_Imemory,
+                               productEmemory = i.prod_Ememory,
+                               productSim = i.prod_sim,
+                               productPin = i.prod_pin,
+                               cateID = i.cate_id
+                           };
+            return View(productDetail);
         }
 
     }
